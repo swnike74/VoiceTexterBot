cbe4096f6a3c79751770d1021b75c6c7abc36112
Ведерников Сергей <swnike@mail.ru>
2025-01-08 20:23:17 +0500

    update 1

--- VoiceTexterBot/Program.cs
+++ VoiceTexterBot/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace VoiceTexterBot
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+
+namespace VoiceTexterBot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("BOT_TOKEN"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
         }
     }
 }
